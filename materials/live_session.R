72631c23caf2f1eac3e1189a0d02af5d4ab9ecee
Felix Kube <F.Kube@students.hertie-school.org>
2024-10-27 13:55:44 +0100

    live session final and more learnr

--- materials/live_session.R
+++ materials/live_session.R
@@ -4,6 +4,8 @@
 ### Author: Felix Kube (2024/10/25)                                           ##
 ################################################################################
 
+### ¯\_(ツ)_/¯ MIND THE QUESTION MAN! When you see him, you should start thinking on your own!
+
 library(tidyverse)
 library(tsibble)
 library(tsibbledata)
@@ -14,19 +16,23 @@ library(feasts)
 # library(fable.binary)
 # library(fable.prophet)
 library(gridExtra) # only for layout purposes in this script
+library(gt) # only for readibility purposes in this script
+library(ggplot2) # only needed to style plots further
 
 # Load data and prepare basics
 data <- tsibbledata::aus_livestock
 
 # Check the data frame for which are the index and key(s)
 str(data)
+interval(data)
 
-### ¯\_(ツ)_/¯ How many rows does the dataset have?
-
+### ¯\_(ツ)_/¯ How many rows does the data set have? What does this mean for the amount of observations?
 "YOUR CODE"
 
 ### ¯\_(ツ)_/¯ How many different time-series does the dataset consist of?
 ### Can you easily descibe them in a table using dplyr::tally()?
+data # %>%
+  # group_by_key(), group_by(), or another way
 
 # To start analizing the data, we first explore the data and identify potential gaps
 has_gaps(data) %>%
@@ -38,53 +44,106 @@ autoplot(data %>% filter(Animal == "Lambs"))
 ### ¯\_(ツ)_/¯ What does this first exploratory plot imply? Try describing using your own words
 print("I conclude that... Therefore, I suspect the...")
 
-# Using STL feature extraction, we can try to figure out the Seasonality of our data
+# Using STL feature extraction, we can try to figure out the seasonality of our data
 data %>%
   filter(Animal == "Lambs") %>%
-  gg_season(Count, period = "year") # seems this depends on a yearly pattern
+  feasts::gg_season(Count, period = "year")
 
 data %>%
   filter(Animal == "Lambs") %>%
-  model(feasts::STL(Count ~ season(window = "periodic")))  %>%
+  feasts::gg_season(Count, period = "6 months")
+
+data %>%
+  filter(Animal == "Lambs") %>%
+  feasts::gg_season(Count, period = "3 months")
+
+data %>%
+  filter(Animal == "Lambs") %>%
+  feasts::ACF(Count) %>%
+  autoplot()
+
+data %>%
+  filter(Animal == "Lambs") %>%
+  model(STL = feasts::STL(Count))  %>%
   components() %>%
   autoplot()
 
 # Let us compare the original data against the 'trend' plot of our STL decomposition
 plot1 <- autoplot(data %>%
-                    filter(Animal == "Lambs")) +
-  theme(legend.position = "none")
+                    filter(Animal == "Lambs",
+                           Month > make_yearmonth(2010, 1))
+                  ) +
+  theme(legend.position = "none") +
+  scale_y_continuous(limits = c(0, 1250000))
 
 plot2 <- data %>%
-  filter(Animal == "Lambs") %>%
-  model(STL(Count ~ season(window = "periodic"))) %>%
+  filter(Animal == "Lambs", Month > make_yearmonth(2010, 1)) %>%
+  model(STL(Count)) %>%
   components() %>%
   select(trend) %>%    # Select only the Trend component
   autoplot() +
-  theme(legend.position = "none")
+  theme(legend.position = "none") +
+  scale_y_continuous(limits = c(0, 1250000))
 
 gridExtra::grid.arrange(plot1, plot2, ncol = 2)
 
 # Let's forecast into the future for Victoria using different models
 models <- data %>%
-  filter(Animal == "Lambs", State == "Victoria") %>%
-  model(ets = ETS(Count ~ trend()),
-        arima = ARIMA(Count ~ trend())) %>%
-  forecast(h = "5 years")
+  filter(Animal == "Lambs") %>%
+  fable::model(ets = ETS(Count ~ trend()),
+               arima = ARIMA(Count ~ trend()))
 
+forecasts <- models %>%
+  fabletools::forecast(h = "5 years")
 
-models %>%
-  filter(.model == "ets") %>%
-  autoplot(data %>% filter(Animal == "Lambs", State == "Victoria")) +
+### ¯\_(ツ)_/¯ What are the classes of the created objects? What are their special properties?
+?tsibble
+"YOUR INVESTIGATION"
+
+forecasts %>%
+  filter(.model == "ets", State %in% c("Victoria", "Tasmania", "New South Wales")) %>%
+  autoplot(data %>% filter(Animal == "Lambs", State %in% c("Victoria", "Tasmania", "New South Wales"))) +
   ggtitle("ETS model")
 
-models %>%
-  filter(.model == "arima") %>%
-  autoplot(data %>% filter(Animal == "Lambs", State == "Victoria")) +
+forecasts %>%
+  filter(.model == "arima", , State %in% c("Victoria", "Tasmania", "New South Wales")) %>%
+  autoplot(data %>% filter(Animal == "Lambs", , State %in% c("Victoria", "Tasmania", "New South Wales"))) +
   ggtitle("ARIMA model")
 
 ### ¯\_(ツ)_/¯ Which of the models seems more realistic?
+# What are each models problems in the short and long term?
 print("I think the model using the...")
+
+# Let's confirm this by looking at model statistics
+glance(models) %>%
+  filter(State %in% c("Victoria", "Tasmania", "New South Wales")) %>%
+  select(-ar_roots, -ma_roots) %>% # only relevant for ARIMA models. Happy if you're interested, but you can read up that on your own as we do not have time. Good luck!
+  gt(groupname_col = "State") %>%
+  tab_style(
+    style = cell_fill(color = "lightgreen"),
+    locations = cells_body(
+      columns = log_lik,
+      rows = log_lik == max(log_lik[State == "Victoria"])
+    )
+  ) %>%
+  tab_style(
+    style = cell_text(color = "red"),
+    locations = cells_body(
+      columns = AIC,
+      rows = AIC == min(AIC[State == "Victoria"])
+    )
+  ) %>%
+  tab_style(
+    style = list(
+      cell_fill(color = "lightgrey"),
+      cell_text(align = "center")
+    ),
+    locations = cells_row_groups()
+  )
 
 ### With the remainder of your time, check out our learnr tutorial
 ### in the GitHub Workshop Classroom or read up on the models at
 ### https://medium.com/analytics-vidhya/time-series-forecasting-models-726f7968a2c1
+
+### ¯\_(^^)_/¯
+# Look! You made the QUESTION MAN happy!
